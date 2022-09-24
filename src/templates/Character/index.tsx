cffcfe8314d7251379f60e48d5531f184d0b1cc2
Junior Alves <jjalves96@gmail.com>
2022-09-24 09:02:11 -0300

    chore: refactor Character page

--- src/templates/Character/index.tsx
+++ src/templates/Character/index.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
+import { BiBookAlt as ComicIcon } from 'react-icons/bi';
+import { BsCalendar3Event as EventsIcon } from 'react-icons/bs';
+import { MdWebStories as StoriesIcon } from 'react-icons/md';
+import { SiBookstack as SeriesIcon } from 'react-icons/si';
 
+import CardInfo from 'components/CardInfo';
 import Heading from 'components/Heading';
-import Tag from 'components/Tag';
+import Line from 'components/Line';
 
 import * as S from './styles';
 
@@ -24,36 +29,61 @@ const Character = ({
   gender,
   location
 }: CharacterProps) => {
+  console.log(image);
+
   return (
     <S.Container>
       <S.Content>
-        <S.HalfPage>
+        <S.InfoContainer>
           <S.ImageContainer>
             <Image
               src={image}
-              loading="lazy"
-              alt={`Thumbnail of ${name}`}
-              placeholder="blur"
               layout="fill"
-              blurDataURL={image}
-              title={name}
+              objectFit="cover"
+              alt={`${name} thumbnail`}
+              title={`${name} thumbnail`}
+              aria-label={`${name} thumbnail`}
             />
           </S.ImageContainer>
-        </S.HalfPage>
-        <S.HalfPage>
-          <S.InfoContainer>
-            <Heading lineLeft lineColor="secundary">
-              {name}
-            </Heading>
 
-            <Tag color={`${status === 'Alive' ? 'green' : 'red'}`}>
-              {status}
-            </Tag>
-            <p>espécie: {species}</p>
-            <p>genêro: {gender}</p>
-            <p>localização: {location.name}</p>
-          </S.InfoContainer>
-        </S.HalfPage>
+          <S.SectionInfo>
+            <Heading lineBottom>{name}</Heading>
+
+            <S.CardContainer>
+              <Line>
+                <CardInfo
+                  href="#"
+                  title="Espécie"
+                  description={species}
+                  icon={ComicIcon}
+                />
+
+                <CardInfo
+                  href="#"
+                  title="Estado"
+                  description={status}
+                  icon={SeriesIcon}
+                />
+              </Line>
+
+              <Line>
+                <CardInfo
+                  href="#"
+                  title="Genêro"
+                  description={gender}
+                  icon={StoriesIcon}
+                />
+
+                <CardInfo
+                  href="#"
+                  title="Localização"
+                  description={location.name}
+                  icon={EventsIcon}
+                />
+              </Line>
+            </S.CardContainer>
+          </S.SectionInfo>
+        </S.InfoContainer>
       </S.Content>
     </S.Container>
   );
