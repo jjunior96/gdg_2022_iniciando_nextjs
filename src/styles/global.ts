c2967fd7a388cb04c6c0a2d24853f9da4446cd7e
Junior Alves <jjalves96@gmail.com>
2022-09-24 08:06:31 -0300

    feat: create styles

---
+++ src/styles/global.ts
@@ -0,0 +1,73 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v15-latin-300.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v15-latin-regular.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: local(''),
+        url('/fonts/montserrat-v15-latin-600.woff2') format('woff2');
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  ${({ theme }) => css`
+    :root {
+      accent-color: ${theme.colors.green};
+    }
+
+    html {
+      font-size: 62.5%;
+    }
+
+    body {
+      font-size: ${theme.font.sizes.medium};
+      background-color: ${theme.colors.white};
+    }
+
+    body,
+    input,
+    textarea,
+    select,
+    button {
+      font-family: ${theme.font.family};
+    }
+  `}
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyles;
